edcb0dd103e3cbebf630be6b87871e443b922892
FaithKamau <faith.kamau@student.moringaschool.com>
2023-06-09 20:23:30 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,8 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
-
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  );
-}
 
 
 function App() {
