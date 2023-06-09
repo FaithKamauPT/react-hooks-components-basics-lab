8b5efeb43661ff06267c54c88de0539a45792149
FaithKamau <faith.kamau@student.moringaschool.com>
2023-06-09 18:51:18 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function NavBar() {
   return (
@@ -8,23 +11,13 @@ function NavBar() {
   );
 }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
